d39f9e30ca35b4d3421696b8bd6946da3b39007f
Adam Harbinson <tgferox@helixnet.co>
2024-11-03 16:06:10 +0000

    Nested select

--- src/couchbase/crud_actions.cr
+++ src/couchbase/crud_actions.cr
@@ -1,16 +1,8 @@
 module CrudActions
   extend self
 
-  def all(collection_name, fields : Array(String) = ["*"])
-    select_string = ""
-
-    fields.each do |v|
-      if v == "*"
-        select_string += " #{collection_name}.#{v} "
-      else
-        select_string += " #{collection_name}.`#{v}` "
-      end
-    end
+  def all(collection_name, fields : Array(String | Hash(String, String) | Hash(String, Array(String))) = ["*"])
+    select_string = gen_search_string(fields, collection_name)
     
     CouchbaseQuery.new(statement: "SELECT META().id AS id, #{select_string} FROM #{Couchbase.settings.bucket_name}.#{Couchbase.settings.scope_name}.#{collection_name};")
   end
@@ -75,30 +67,14 @@ module CrudActions
     CouchbaseQuery.new(statement: statement, args: process_args(conditions.values))
   end
 
-  def select_by_id(collection_name, id, fields : Array(String) = ["*"])
-    select_string = ""
-
-    fields.each do |v|
-      if v == "*"
-        select_string += " #{collection_name}.#{v} "
-      else
-        select_string += " #{collection_name}.`#{v}` "
-      end
-    end
+  def select_by_id(collection_name, id, fields : Array(String | Hash(String, String) | Hash(String, Array(String))) = ["*"])
+    select_string = gen_search_string(fields, collection_name)
 
     CouchbaseQuery.new "SELECT META().id AS id, #{select_string} FROM #{Couchbase.settings.bucket_name}.#{Couchbase.settings.scope_name}.#{collection_name} USE KEYS \"#{id}\";"
   end
 
-  def select_by(collection_name, conditions, fields : Array(String) = ["*"])
-    select_string = ""
-
-    fields.each do |v|
-      if v == "*"
-        select_string += " #{collection_name}.#{v} "
-      else
-        select_string += " #{collection_name}.`#{v}` "
-      end
-    end
+  def select_by(collection_name, conditions, fields : Array(String | Hash(String, String) | Hash(String, Array(String))) = ["*"])
+    select_string = gen_search_string(fields, collection_name)
 
     where_clause = conditions.map do |k, v|
       key = k.to_s == "id" ? "META(#{collection_name}).id" : "`#{k}`"
@@ -114,16 +90,8 @@ module CrudActions
     CouchbaseQuery.new(statement: statement, args: process_args(conditions.values))
   end
 
-  def select(collection_name, statement, args, fields : Array(String) = ["*"])
-    select_string = ""
-
-    fields.each do |v|
-      if v == "*"
-        select_string += " #{collection_name}.#{v} "
-      else
-        select_string += " #{collection_name}.`#{v}` "
-      end
-    end
+  def select(collection_name, statement, args, fields : Array(String | Hash(String, String) | Hash(String, Array(String))) = ["*"])
+    select_string = gen_search_string(fields, collection_name)
 
     statement = "SELECT META().id AS id, #{select_string} FROM #{Couchbase.settings.bucket_name}.#{Couchbase.settings.scope_name}.#{collection_name} WHERE #{statement};"
 
@@ -134,5 +102,31 @@ module CrudActions
     JSON.parse(
       args.to_json
     )
+  end
+
+  private def gen_search_string(fields, collection_name)
+    select_string = ""
+
+    fields.each do |v|
+      if v.is_a?(Hash)
+        v.each do |inner_k, inner_v|
+          values = [] of String
+          if inner_v.is_a?(String)
+            values << "'#{inner_v}': #{collection_name}.`#{inner_k}`.`#{inner_v}` "
+          else
+            inner_v.each do |inner_innver_v|
+              values << "'#{inner_innver_v}': #{collection_name}.`#{inner_k}`.`#{inner_innver_v}` "
+            end
+          end
+          select_string += "{#{values.join(",")}} as #{inner_k} "
+        end
+      elsif v == "*"
+        select_string += " #{collection_name}.#{v} "
+      else
+        select_string += " #{collection_name}.`#{v}` "
+      end
+    end
+
+    return select_string
   end
 end
